364444952c9f53864ab1094d27ca89a6b8c43314
Mhistersax <bamigboyepelumi38@gmail.com>
2022-04-20 10:28:43 +0100

    init

--- FILE HANDLING/main3.c
+++ FILE HANDLING/main3.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdbool.h>
+
+
 #define max 1000
+
 int main(){
 FILE *fp;
 char buffer[max];
+
 fp = fopen("text.txt","a");
 if(fp == NULL)
 {
     printf("ERROR OPENING FILE!!\n");
-    
+    return 1;
 }
+printf("enetr kill to quite\n");
 do
 {
     fgets(buffer,max,stdin);
-    if (strcmp(buffer, "kill\n") ==0);
+    if (strcmp(buffer, "kill\n") == 0)
         break;
-    fputs(buffer,fp);
+    fputs(buffer, fp);
 
 } while (true);
 
