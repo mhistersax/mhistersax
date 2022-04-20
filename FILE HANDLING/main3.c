3b82e1bf10551d0f287caeebaa5994ffe3274eaf
Mhistersax <bamigboyepelumi38@gmail.com>
2022-04-20 10:12:29 +0100

    INIT

---
+++ FILE HANDLING/main3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+#define max 1000
+int main(){
+FILE *fp;
+char buffer[max];
+fp = fopen("text.txt","a");
+if(fp == NULL)
+{
+    printf("ERROR OPENING FILE!!\n");
+    
+}
+do
+{
+    fgets(buffer,max,stdin);
+    if (strcmp(buffer, "kill\n") ==0);
+        break;
+    fputs(buffer,fp);
+
+} while (true);
+
+fclose(fp);
+    return 0;
+}
